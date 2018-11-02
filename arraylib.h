2ed6d6fd1104db470781ee5915e4e8ce0c9137ef
zxf20180725 <zhaoxiongfei6789@163.com>
2018-11-02 11:14:57 +0800

    新增文件

---
+++ arraylib.h
@@ -0,0 +1,21 @@
+﻿#ifndef  _ARRAY_LIB_H_
+#define  _ARRAY_LIB_H_
+
+typedef struct 
+{
+	int  n;     // 动态数组的元素个数
+	//int  size;	// 动态数组的元素空间大小
+	void **data; // 动态数组的数据起始地址
+} Array;
+
+/*
+	功能：初始化动态数组
+	参数：
+		int n:数组长度
+		void(*init_obj)(void*):初始化元素的回调函数
+	返回值：
+		Array指针或0
+*/
+Array *init_array(int n);
+
+#endif  /* _ARRAY_LIB_H_ */
